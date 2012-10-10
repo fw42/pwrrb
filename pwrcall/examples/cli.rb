fb7dc0054b0f879e23f21c830f83592a1750ae67
Florian Weingarten <flo@hackvalue.de>
2012-10-10 14:06:49 +0200

    pwrcall (non-blocking) command line interface! pry is awesome! :-)

---
+++ pwrcall/examples/cli.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'rb-readline'
+require 'pry'
+require File.expand_path("../../pwrcall.rb", __FILE__)
+require File.expand_path("../nonblocking_keyboard.rb", __FILE__)
+
+Pwr.run do
+	PwrFiber.new{
+		EM.open_keyboard(NonblockingKeyboard) do |kb|
+			node = PwrNode.new()
+			obj, pwr = node.open_url(
+				"pwrcall://21bc7f3c3956e5aa04a6dc33fea9d2b913b4157c@localhost:10001/foobar"
+			)
+			exit unless pwr
+			Pry.prompt = [
+				proc { |obj, nest_level| "pwr> " },
+				proc { |obj, nest_level| "pwr> " }
+			]
+			binding.pry({ :quiet => true })
+		end
+	}.resume().wait()
+	Pwr.stop()
+end
---
+++ pwrcall/examples/nonblocking_keyboard.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# http://www.simulacre.org/unblocking-readline-in-eventmachine/
+require 'fiber'
+require 'eventmachine'
+
+module NonblockingKeyboard
+	def post_init
+		@ostdin = $stdin
+		$stdin  = self
+		@buffer = ""
+	end
+
+	def receive_data(d)
+		@buffer << d
+		@waiting && @waiting[:cnt] <= @buffer.length && @waiting[:fiber].resume
+	end
+
+	def read(cnt)
+		if @buffer.length < cnt
+			@waiting = {:cnt => cnt, :fiber => Fiber.current}
+			Fiber.yield
+		end
+		data, @buffer = @buffer[0...cnt], @buffer[cnt..-1]
+		data
+  end
+
+	def unbind
+		$stdin = @ostdin
+	end
+
+	def method_missing(meth, *args, &blk)
+		@ostdin.send(meth, *args, &blk)
+	end
+end
