ecfcad8d7c1f61a7ff273b09e849627292fcbb82
Johannes Gilger <heipei@hackvalue.de>
2012-10-26 16:13:04 +0200

    refactoring

--- pwrcall/websocket/static/index.js
+++ pwrcall/websocket/static/index.js
@@ -3,125 +3,145 @@
 // globals
 var Socket = window.MozWebSocket || window.WebSocket;
 
-pwr_caps = {};            // cap hash
-msg_id = 0;               // global message id
-callbacks = [];           // array to hold callback methods
+var pwr_ns = function() {
+  this.caps = {};
+  this.callbacks = [];
+  this.msg_id = 0;
 
-// websocket functions
-function ws_connect(url) {
-  socket = new Socket(url);
+  // pwr. functions {{{
+  this.add_cap = function(cap, host) {
+    if(!this.caps[cap]) {
+      this.caps[cap] = new this.pwr_cap(cap,host);
+    }
+  }
 
-  // Bind socket methods
-  socket.addEventListener('open', function() {
-    log('-- Connected to ' + socket.URL);
-    setInterval(function() { pwr_call.apply("%ping",["ping"]) }, 2000);
-  });
-
-  socket.onerror = function(event) {
-    log('!! ERROR: ' + event.message);
-  };
-
-  socket.onopen = function(event) {
-    $("span#state").text("Connected!");
-  };
-
-  socket.onmessage = function(event) {
-    if(event.data.indexOf("pwrcall") == 0) {
-      pwr_hello();
-      return;
+  this.handle_reply = function (reply) {
+    if(reply.error) {
+      if(!reply.error.match(/%ping/)) {
+        log('<< pwrcall error: ' + reply.error);
+      }
     } else {
-      pwr = pwr_parse(event.data);
-      switch(pwr["type"]) {
-        case 0:
-          pwr_call_handler(pwr);
-          return;
-        case 1:
-          pwr_reply_handler(pwr);
-          return;
-        default:
-          log("!! unknown pwrcall message type " + pwr["type"]);
+      log('<< pwrcall reply: ' + reply.result);
+      if("undefined" != typeof this.callbacks[reply.msg_id]) {
+        this.callbacks[reply.msg_id](reply.result);
       }
     }
   };
 
-  socket.onclose = function(event) {
-    log('!! CLOSE: ' + event.code + ', ' + event.reason);
-    $("span#state").text("Disconnected...");
-    $("span#state").append($("<input type='submit'/ value='Reconnect'>").attr("onclick", "ws_connect('" + url + "');"));
+  this.hello = function(socket) {
+      socket.send("pwrcall pwrcalljs_v0.1 - caps: json\n");
   };
+
+  this.parse = function(string) {
+    var pwr_array = JSON.parse(string);
+    var pwr_msg = {"type": pwr_array[0], "msg_id": pwr_array[1]};
+    switch(pwr_msg["type"]) {
+      case(0):
+        pwr_msg["cap"] = pwr_array[2];
+        pwr_msg["fct"] = pwr_array[3];
+        pwr_msg["args"] = pwr_array[4];
+      case(1):
+        pwr_msg["error"] = pwr_array[2];
+        pwr_msg["result"] = pwr_array[3];
+    }
+    return pwr_msg;
+  }
+  // }}}
+
+  // {{{ pwr_cap
+  this.pwr_cap = function(cap, host) {
+    this.cap = cap;
+    this.ws = new pwrsock(host);
+
+    this.pcall = function (fct, args, callback) {
+      if(!fct.match(/ping/))
+        log('>> pwrcall request: ' + fct + "(" + args + ")"); 
+
+      this.ws.socket.send(JSON.stringify([0,pwr.msg_id,this.cap,fct,args]));
+      pwr.callbacks[pwr.msg_id] = callback;
+      pwr.msg_id++;
+    };
+
+    this.reply = function (msg_id, error, result) {
+      log('>> pwrcall reply: ' + result + " (error: " + error + ")"); 
+      this.ws.socket.send(JSON.stringify([1,msg_id,error,result]));
+    };
+
+    this.handle_call = function (msg) {
+      log('<< msgcall request: ' + msg.fct + "(" + msg.args + ")"); 
+        if (this[msg.fct]) {
+          msg_return = this[msg.fct].apply(this,msg.args);
+          this.reply(msg["msg_id"], undefined, msg_return);
+        } else {
+          log("!! I don't have function " + msg.fct);
+        }
+    };
+  }
+  //}}}
+};
+
+var pwr = new pwr_ns();
+
+// websocket functions {{{
+var pwrsock = function(url) {
+
+  // look if another cap has the same WS
+  for(var cap in pwr.caps) {
+    if(pwr.caps[cap] && pwr.caps[cap].ws && (pwr.caps[cap].ws.socket.URL == url)) {
+      this.socket = pwr.caps[cap].ws.socket;
+      break;
+    }
+  }
+
+  if(!this.socket) {
+    this.socket = new Socket(url);
+
+    this.socket.addEventListener('open', function() {
+      log('-- Connected to ' + this.URL);
+      var s = this;
+      setInterval(function() { s.send(JSON.stringify([0, 1, "%ping",["ping"]])) }, 2000);
+    });
+
+    this.socket.onerror = function(event) {
+      log('!! ERROR: ' + event.message);
+    };
+
+    this.socket.onopen = function(event) {
+      $("span#state").text("Connected!");
+    };
+
+    this.socket.onmessage = function(event) {
+      if(event.data.indexOf("pwrcall") == 0) {
+        pwr.hello(this);
+        return;
+      }
+
+      var msg = pwr.parse(event.data);
+      switch(msg["type"]) {
+        case 0:
+          var cap = pwr.caps[msg["cap"]];
+          cap.handle_call(msg);
+          return;
+        case 1:
+          pwr.handle_reply(msg);
+          return;
+        default:
+          log("!! unknown pwrcall message type " + msg["type"]);
+      }
+    };
+
+    this.socket.onclose = function(event) {
+      log('!! CLOSE: ' + event.code + ', ' + event.reason);
+      $("span#state").text("Disconnected...");
+      $("span#state").append($("<input type='submit'/ value='Reconnect'>").attr("onclick", "ws_connect('" + url + "');"));
+    };
+  }
+
 }
 
+// }}}
 
-// pwr functions
-function pwr_hello() {
-  socket.send("pwrcall pwrcalljs_v0.1 - caps: json\n");
-};
-
-function pwr_parse(string) {
-  pwr_array = JSON.parse(string);
-  pwr = {"type": pwr_array[0], "msg_id": pwr_array[1]};
-  switch(pwr["type"]) {
-    case(0):
-      pwr["cap"] = pwr_array[2];
-      pwr["fct"] = pwr_array[3];
-      pwr["args"] = pwr_array[4];
-    case(1):
-      pwr["error"] = pwr_array[2];
-      pwr["result"] = pwr_array[3];
-  }
-  return pwr;
-}
-
-function pwr_call(fct, args, callback) {
-  if(!fct.match(/ping/))
-    log('>> pwrcall request: ' + fct + "(" + args + ")"); 
-
-  socket.send(JSON.stringify([0,msg_id,this,fct,args]));
-  callbacks[msg_id] = callback;
-  msg_id++;
-};
-
-function pwr_reply(msg_id, error, result) {
-  log('>> pwrcall reply: ' + result + " (error: " + error + ")"); 
-  socket.send(JSON.stringify([1,msg_id,error,result]));
-};
-
-function pwr_reply_handler(pwr) {
-  if(pwr.error) {
-    if(!pwr.error.match(/%ping/)) {
-      log('<< pwrcall error: ' + pwr.error);
-    }
-  } else {
-    log('<< pwrcall reply: ' + pwr.result);
-    if("undefined" != typeof callbacks[pwr.msg_id]) {
-      callbacks[pwr.msg_id](pwr.result);
-    }
-  }
-};
-
-function pwr_call_handler(pwr) {
-  log('<< pwrcall request: ' + pwr.fct + "(" + pwr.args + ")"); 
-  if (pwr_caps[pwr.cap]) {
-    if (pwr_caps[pwr.cap][pwr.fct]) {
-      pwr_return = pwr_caps[pwr.cap][pwr.fct].apply(this,pwr.args);
-      pwr_reply(pwr.msg_id, undefined, pwr_return);
-    } else {
-      log("!! I don't have function " + pwr.fct);
-    }
-  } else {
-    log("!! I don't have capability " + pwr.cap);
-  }
-};
-
-function pwr_open_ref(ref) {
-  pwr_caps[ref] = {};
-  pwr_caps[ref].pwr_call = function(call) {
-    pwr_call.apply(ref, call);
-  };
-}
-
-
-// ui functioniality
+// ui functioniality {{{
 function send_command() {
   var string = $("#command").val();
   var cap = string.split(".")[0];
@@ -134,12 +154,13 @@ function send_command() {
     args = [];
   }
 
-  if(pwr_caps[cap]) {
-    pwr_caps[cap].pwr_call([command, args]);
+  if(pwr.caps[cap]) {
+    pwr.caps[cap].pcall(command, args);
   }
 }
+//}}}
 
-// log
+// log// {{{
 function log(text) {
   $("div#log").prepend(text+"\n");
 };
@@ -169,19 +190,20 @@ var public_methods = {
   log: log()
 }
 
-pwr_open_ref("example");
-pwr_open_ref("browser");
-
-// put public methods into capability
-for(var fct in public_methods) {
-  pwr_caps.browser[fct] = public_methods[fct];
-};
-
 $(document).ready(function() {
   
   for(var fct in public_methods) {
     $("#caps ul").append($("<li/>").text(fct));
   };
 
-  ws_connect("ws://localhost:3001");    // connect to this websocket
+  pwr.add_cap("example", "ws://localhost:3001/");
+  pwr.add_cap("browser", "ws://localhost:3001/");
+
+  // put public methods into browser capability
+  for(var fct in public_methods) {
+    pwr.caps.browser[fct] = public_methods[fct];
+  };
+
+
 });
+//}}}
